911ab1f7b730f16dcae1750cbc6dd6c35b4614be
dkhurlop <denisekhurlopian@gmail.com>
2023-05-18 16:58:55 +0200

    hitthedrum

--- main.js
+++ main.js
@@ -7,3 +7,14 @@ function makeSound(id){
     audio.play();
 
   }
+
+  toggle.onclick = () =>{
+    items.classList.toggle("open");
+    toggle.classList.toggle("close");
+  }
+  const sonidos = document.querySelector(".big-pad")
+  let mysound;
+  sonidos.addEventListener("click", () => {
+    mysound = new Audio('/sound/bombo.wav');
+    mysound.play()
+  })
